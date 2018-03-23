f9f3e06f3887f854a4ca1f12b4ce44f9175532f6
Shubham Rai <shubham.rai@shop101.com>
2018-03-23 18:21:26 +0530

    App wrapped up

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+// Import the libraries to help create a component
+import React from 'react';
+import { View, AppRegistry } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
+// Create a component
+const App = () => (
+  <View style={{ flex: 1 }} >
+    <Header headerText='Songs' />
+    <AlbumList />
+  </View>
+);
+
+//Render it to the device
 AppRegistry.registerComponent('albums', () => App);
